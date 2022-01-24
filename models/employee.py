30652a2b2ef180dd5d579627d79c59f79457f586
jonma <jonma@192.168.0.18>
2022-01-24 23:18:49 +0100

    avance desarrollo clase employee

--- models/employee.py
+++ models/employee.py
@@ -1,13 +1,25 @@
-
-
-from odoo import models,fields,api
+from odoo import api
+from odoo import fields
+from odoo import models
+from odoo.exceptions import ValidationError
+import re
 
 class Employee(models.Model):
-    _name='java_gaming.employee'
-    _inherit='res.users'
+    _name = 'java_gaming.employee'
+    _inherit = 'res.users'
    
-    fechaContratacion = fields.Date()
-    salario = fields.Float()  
+    fechaContratacion = fields.Date(default=fields.Date.today)
+    salario = fields.Float(string="Pajin")  
+    
 
 
-    games=fields.Many2many('java_gaming.game',String="Game")
+
+    games = fields.Many2many('java_gaming.game', String="Game")
+    
+        @api.onchange('email_id')
+        def validate_mail(self):
+       if self.email_id:
+        match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', self.email_id)
+        if match == None:
+            raise ValidationError('Not a valid E-mail ID')
+        
